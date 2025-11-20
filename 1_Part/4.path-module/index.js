7e1bd39b8612fd0aab5af7ef68f0b2174a1f3711
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-20 21:03:21 +0530

    learning about path how to check cd aur many thing

---
+++ 1_Part/4.path-module/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+
+console.log("Directory name:",path.dirname(__filename),);
+console.log("File name",path.basename(__filename));
+console.log("File Extension",path.extname(__filename))
+
+const joinPath = path.join('/user','document','node','projects')
+console.log("Joind path:",joinPath)
+
+const resolvePath = path.resolve('user','documents','node','projects')
+console.log("Resolve path",resolvePath)
+
+const normalizePath = path.normalize('/user/.documents/../node/projects')
+console.log("normalizePath:",normalizePath)

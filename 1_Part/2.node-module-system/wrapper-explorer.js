11c62c4c16b10fdb9559441613a4b8b8ea9af463
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-20 19:04:08 +0530

    Learning about backend in js

---
+++ 1_Part/2.node-module-system/wrapper-explorer.js
@@ -0,0 +1,10 @@
+
+
+console.log('Node module wrapper demo ');
+console.log('__filename in wrapper demo',__filename)
+console.log('__filename in wrapper demo',__dirname)
+
+
+module.exports.greet = function(name){
+    console.log(`Hello ${name}`)
+}

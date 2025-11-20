7edefab58bfd2096c5873e065c96cae329aaae48
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-20 18:34:18 +0530

    Learning about backend in js

--- 1_Part/1.Hello_World_node/index.js
+++ 1_Part/1.Hello_World_node/index.js
@@ -1,1 +1,12 @@
 console.log("Hello world node")
+
+let array =[1,2,3,4,5]
+
+console.log("array",array)
+
+setTimeout(()=>{
+    console.log("this message is delayed by 3 second")
+},3000)
+
+console.log("this code is sync it work before async")
+

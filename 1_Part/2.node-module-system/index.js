6464b0fe6f0fccd0858a87e002ec17c6775977e1
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-20 18:47:51 +0530

    Learning about backend in js

--- 1_Part/2.node-module-system/index.js
+++ 1_Part/2.node-module-system/index.js
@@ -6,3 +6,13 @@
 const firstModule = require("./first-module")
 
 console.log(firstModule.add(10,25))
+
+
+try{
+    console.log("")
+    let divison_result = firstModule.divide(0,1)
+    console.log(divison_result)
+
+}catch(error){
+console.log("caught an error",error.message);
+}

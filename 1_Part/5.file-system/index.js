d5837ce6ea02a107556b30f0a07be6ef198f1e0b
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-21 09:23:36 +0530

    Learning about how u can chnage file and read there content

--- 1_Part/5.file-system/index.js
+++ 1_Part/5.file-system/index.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const fs = require('fs')
 
 const path = require('path');
@@ -16,6 +17,27 @@ console.log("file created successfully!")
 const readContentFromFile = fs.readFileSync(filePath,'utf8');
 console.log("file Content:",readContentFromFile)
 
-
+//to added new line in existing file
 fs.appendFileSync(filePath,'\n this is a new line added to that file ')
 console.log("new file content added");
+
+//async way to creating the file
+
+const asyncFilePath = path.join(dataFolder,'async-example.txt')
+fs.writeFile(asyncFilePath,"hello async node js",(err)=>{
+    if(err) throw err;
+    console.log('Async file is created successfully')
+    fs.readFile(asyncFilePath,'utf8',(err,data)=>{
+        if(err) throw err;
+        console.log('Asyncs file content:',data)
+    })
+    fs.appendFile(asyncFilePath,'\n this is new line in asyncfile',(err)=>{
+        if(err) throw err;
+        console.log("New line added in the file")
+        fs.readFile(asyncFilePath,'utf8',(err,updateddata)=>{
+            if(err) throw err;
+            console.log("updated content read",updateddata)
+        })
+    })
+})
+

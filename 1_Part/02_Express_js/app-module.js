b66f5e0690491ed624d996abdffaa97cebaee44e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-22 12:32:34 +0530

    express js

---
+++ 1_Part/02_Express_js/app-module.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+
+const app = express();
+
+//application level setting
+app.set('view engine','ejs')
+
+//routing
+app.get('/',(req,res)=>{
+    res.send('Home page')
+})
+app.get('./products',(req,res)=>{
+    res.send("product page")
+})
+
+app.post('/api/data',(req,res)=>{
+    res.json({
+        message:'Data recevied',
+        data:req.body,
+    })
+})
+
+app.use((err,req,res,next)=>{
+    console.log(err.stack)
+    res.status(500).send('Somthing went wrong')
+})

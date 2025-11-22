7719536f9e3d507b50e51cb95c43bbf775a187fc
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-22 18:15:27 +0530

    Learning about dynamic routing in js

--- 1_Part/02_Express_js/1_index.js
+++ 1_Part/02_Express_js/1_index.js
@@ -4,15 +4,11 @@ const express = require('express');
 
 const app = express();
 app.get('/',(req,res)=>{
-    res.send("Hello world");
-})
-
-app.get('/products',(req,res)=>{
-    res.send("List of products")
+    res.send("Hello world and too you ");
 })
 
 
-const Port = 3000;
-app.listen(Port,()=>{
-    console.log(`Port is runing on ${Port}`)
-})
+// const Port = 3000;
+// app.listen(Port,()=>{
+//     console.log(`Port is runing on ${Port}`)
+// })

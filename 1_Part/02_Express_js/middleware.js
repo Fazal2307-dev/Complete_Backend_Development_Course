0e05df3b980bebc79ae70c51cd2ace9591f0672f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-11-23 10:55:37 +0530

    middleware

---
+++ 1_Part/02_Express_js/middleware.js
@@ -0,0 +1,25 @@
+
+const express = require('express')
+
+const app = express();
+
+
+//define middleware function
+const myFirstMiddleware = (req,res,next)=>{
+console.log("this first middleware will run on every request");
+
+next();
+}
+app.use(myFirstMiddleware);
+
+app.get('/',(req,res)=>{
+    res.send("home page")
+
+})
+
+app.get('/about',(req,res)=>{
+    res.send("about page")
+})
+
+app.listen(3000,()=>{
+    console.log("server is runing on Prot 3000")})
